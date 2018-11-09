6fbca3b2fc08890280f3ba1c1704e64f040e8e78
apple <reubenastone@gmail.com>
2018-11-09 10:50:26 +0800

    updated styles and added label to strength meter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import PasswordInput from './components/password-input'
 
 class App extends Component {
+
+  constructor() {
+  	super()
+  	this.state = {
+  		password: ''
+  	}
+  	this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(event, attr) {
+  	const newState = { ...this.state }
+  	newState[attr] = event.target.value
+  	this.setState(newState)
+  }
+
   render() {
     return (
       <div className="App">
-
+      	<PasswordInput
+      		value={this.state.password}
+      		placeholder="Your secure password"
+      		handleChange={(e) => this.handleChange(e, 'password')}
+      	/>
       </div>
     );
   }
